b20dfa5997f8bf83f0403de9f6757097a6d1528b
Phil Azar <azar.phil@gmail.com>
2020-03-16 21:47:00 -0400

    adding lines

--- scrape.R
+++ scrape.R
@@ -13,14 +13,28 @@ politicians <- c( 'GovMikeDewine',
                  'CDCgov')
 
 raw_tweets <- rtweet::get_timeline(politicians, n = 1500)
+
+
+cols = names(raw_tweets)
 for(i in 1:length(cols)){
   if(class(raw_tweets[[i]]) == 'list'){
     raw_tweets[[i]] <- vapply(raw_tweets[[i]], paste, collapse = ", ", character(1L))
   }
 }
 write.csv(raw_tweets, 'pols_tweets.csv')
-trump_tweets <- rtweet::get_timeline('realDonaldTrump', n=3200)
 
-cols = names(raw_tweets)
+# need to wait a day for api requests 
+trump_tweets <- rtweet::get_timeline('realDonaldTrump', n=3200)
+cols = names(trump_tweets)
+for(i in 1:length(cols)){
+  if(class(trump_tweets[[i]]) == 'list'){
+    trump_tweets[[i]] <- vapply(trump_tweets[[i]], paste, collapse = ", ", character(1L))
+  }
+}
+write.csv(trump_tweets, 'trumps_tweets.csv')
+
+
+
+
 
 
